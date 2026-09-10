adbf00cfe4878451ab4670f9993bcae8f0a0688b
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-09-10 17:02:43 +0700

    Aktifkan fallback foto Sukarno di halaman profil

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { mount } from "svelte";
 import "./inti/kontak-resmi.js";
 import App from "./App.svelte";
 import "./gaya/global.css";
+import "./gaya/perbaikan-pengurus.css";
 
 export default mount(App, { target: document.getElementById("app") });
