13d13143359fc02cb5f5fbd0f4d9840cf1be0081
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-09-11 18:25:03 +0700

    Tambahkan identitas akun petugas siap pakai

---
+++ src/inti/akses.js
@@ -0,0 +1,12 @@
+/**
+ * Akun operasional petugas yang disiapkan untuk pemakaian bersama RW 02.
+ *
+ * Kata sandi TIDAK pernah disimpan di repository. Petugas tetap mengetik
+ * kata sandinya di halaman Masuk dan Firebase Authentication yang memeriksa.
+ */
+export const PETUGAS_SIAP_PAKAI = Object.freeze({
+  email: "petugas02@gmail.com",
+  nama: "Petugas RW 02",
+  jabatan: "Petugas RW",
+  peran: "petugas"
+});
