1c757b563a14a2c86cb553efcba84385dd65d7ec
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-09-15 18:11:35 +0700

    Aktifkan sinkronisasi konten publik

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { mount } from "svelte";
 import "./inti/kontak-resmi.js";
 import { aktifkanInteraksiUi } from "./inti/interaksi-ui.js";
+import { aktifkanInteraksiKonten } from "./keadaan/interaksi-konten.js";
 import App from "./App.svelte";
 import "./gaya/global.css";
 import "./gaya/perbaikan-pengurus.css";
 
 const aplikasi = mount(App, { target: document.getElementById("app") });
 aktifkanInteraksiUi();
+aktifkanInteraksiKonten();
 
 export default aplikasi;
