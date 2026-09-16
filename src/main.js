dc2a9becc89667460ad369d9a897034ca3f4fec2
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-09-17 01:07:28 +0700

    Pulihkan entry point tanpa eksperimen layout runtime

--- src/main.js
+++ src/main.js
@@ -2,18 +2,15 @@ import { mount } from "svelte";
 import "./inti/kontak-resmi.js";
 import { aktifkanInteraksiUi } from "./inti/interaksi-ui.js";
 import { aktifkanPeningkatanPortal } from "./inti/peningkatan-portal.js";
-import { aktifkanLayoutInformasi } from "./inti/layout-informasi.js";
 import { aktifkanInteraksiKonten } from "./keadaan/interaksi-konten.js";
 import App from "./App.svelte";
 import "./gaya/global.css";
 import "./gaya/perbaikan-pengurus.css";
 import "./gaya/surat-bersih.css";
-import "./gaya/informasi-layout-tetap.css";
 
 const aplikasi = mount(App, { target: document.getElementById("app") });
 aktifkanInteraksiUi();
 aktifkanInteraksiKonten();
 aktifkanPeningkatanPortal();
-aktifkanLayoutInformasi();
 
 export default aplikasi;
